311a99824d59f7a7db7089e7c1ab7cc2c72faca8
zengrui <2531594414@qq.com>
2025-06-17 12:20:28 +0800

    feat(addCommentCount)

--- src/main/java/com/zengrui/zblog/server/mapper/BlogMapper.java
+++ src/main/java/com/zengrui/zblog/server/mapper/BlogMapper.java
@@ -4,6 +4,7 @@ import com.zengrui.zblog.pojo.entity.Blog;
 import com.zengrui.zblog.pojo.vo.BlogListVO;
 import com.zengrui.zblog.pojo.vo.ReadBlogVO;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Update;
 
 import java.util.List;
 
@@ -16,4 +17,7 @@ public interface BlogMapper {
     List<BlogListVO> getAllBlogs();
 
     List<BlogListVO> getMineBlogs(Long id);
+
+    @Update("update blog set comment_count = comment_count + 1 where id = #{blogId}")
+    void addComment(Long blogId);
 }
--- src/main/java/com/zengrui/zblog/server/service/impl/CommentServiceImpl.java
+++ src/main/java/com/zengrui/zblog/server/service/impl/CommentServiceImpl.java
@@ -4,7 +4,9 @@ import com.zengrui.zblog.pojo.dto.CommentAddDTO;
 import com.zengrui.zblog.pojo.entity.Comment;
 import com.zengrui.zblog.pojo.vo.CommentListVO;
 import com.zengrui.zblog.pojo.vo.UserViewVO;
+import com.zengrui.zblog.server.mapper.BlogMapper;
 import com.zengrui.zblog.server.mapper.CommentMapper;
+import com.zengrui.zblog.server.service.BlogService;
 import com.zengrui.zblog.server.service.CommentService;
 import com.zengrui.zblog.server.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,11 +21,15 @@ public class CommentServiceImpl implements CommentService {
     private CommentMapper commentMapper;
 
     @Autowired
+    private BlogMapper blogMapper;
+
+    @Autowired
     private UserService userService;
 
     @Override
     public void add(CommentAddDTO commentAddDTO) {
         UserViewVO userViewVO = userService.view(commentAddDTO.getAuthorId());
+        blogMapper.addComment(commentAddDTO.getBlogId());
         String username = userViewVO.getUsername();
         Comment comment = Comment.builder()
                 .author(username)
